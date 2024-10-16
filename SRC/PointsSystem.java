8e35013b88438c580877ebe454fb2c0f897ac3e4
JohnPapadat0s <161411675+JohnPapadat0s@users.noreply.github.com>
2024-10-16 14:03:42 +1100

    added point system objects

--- SRC/PointsSystem.java
+++ SRC/PointsSystem.java
@@ -1,5 +1,14 @@
 package SRC;
 
 public class PointsSystem {
-    
+    public int Win;
+    public int Draw;
+    public int Loss;
+
+    public PointsSystem(int Win, int Draw, int Loss){
+        this.Win = Win;
+        this.Draw = Draw;
+        this.Loss = Loss;
+
+    }
 }
