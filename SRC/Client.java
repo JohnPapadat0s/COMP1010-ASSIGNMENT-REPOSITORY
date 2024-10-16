5d1178042cfa6bdd498fc5cbd881eb1bc082030a
HarrisonCove0 <harrison.cove@students.mq.edu.au>
2024-10-16 15:23:40 +1100

    player

--- SRC/Client.java
+++ SRC/Client.java
@@ -9,5 +9,23 @@ public class Client {
 
     public static void main(String args[]){
         Players Player1 = new Players(random.nextInt(100), "Team");
+        Players Player2 = new Players(random.nextInt(100), "Team");
+        Players Player3 = new Players(random.nextInt(100), "Team");
+        Players Player4 = new Players(random.nextInt(100), "Team");
+
+        Players Player5 = new Players(random.nextInt(100), "Team");
+        Players Player6 = new Players(random.nextInt(100), "Team");
+        Players Player7 = new Players(random.nextInt(100), "Team");
+        Players Player8 = new Players(random.nextInt(100), "Team");
+        
+        Players Player9 = new Players(random.nextInt(100), "Team");
+        Players Player10 = new Players(random.nextInt(100), "Team");
+        Players Player11= new Players(random.nextInt(100), "Team");
+        Players Player12= new Players(random.nextInt(100), "Team");
+        
+        Players Player13 = new Players(random.nextInt(100), "Team");
+        Players Player14 = new Players(random.nextInt(100), "Team");
+        Players Player15 = new Players(random.nextInt(100), "Team");
+        Players Player16 = new Players(random.nextInt(100), "Team");
 }
 }
