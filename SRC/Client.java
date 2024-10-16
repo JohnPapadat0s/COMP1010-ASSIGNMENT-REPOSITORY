1327fb5f07601e3e284b19d3dad1c11e09e59cdd
HarrisonCove0 <harrison.cove@students.mq.edu.au>
2024-10-16 13:27:43 +1100

    COommittt

--- SRC/Client.java
+++ SRC/Client.java
@@ -1,15 +1,9 @@
 package SRC;
-
 import java.util.List;
 import java.util.ArrayList;
 
 public class Client {
     public static void main(String args[]){
-        Player player1 = new Player("Sid", "Homosexual", 19);
-        Player.displayPlayerInfo(player1);
-
-        List<Player> team1 = new ArrayList<Player>();
-        team1.add(player1);
-        Team Macquarie = new Team("Macquarie", team1 ,"Macquarie Uni");
+        Team Macquarie = new Team("Macquarie", "Macquarie Uni");
     }
 }
--- SRC/Team.java
+++ SRC/Team.java
@@ -1,16 +1,13 @@
 package SRC;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
     public String teamName;
-    public List<Player> listOfPlayers = new ArrayList<>();
     public String homeStadium;
 
-    public Team(String teamName, List<Player> listOfPlayers, String homeStadium){
+    public Team(String teamName, String homeStadium){
         this.teamName = teamName;
-        this.listOfPlayers = listOfPlayers;
         this.homeStadium = homeStadium;
     }
 
