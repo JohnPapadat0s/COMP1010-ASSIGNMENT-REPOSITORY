941ee3fd29468ac22d785c962a6abf203cee9274
HarrisonCove0 <harrison.cove@students.mq.edu.au>
2024-10-16 17:48:26 +1100

    Random Name creations of players and teams

--- SRC/Client.java
+++ SRC/Client.java
@@ -8,24 +8,72 @@ public class Client {
      int playerSkill = random.nextInt(100);
 
     public static void main(String args[]){
-        Players Player1 = new Players(random.nextInt(100), "Team");
-        Players Player2 = new Players(random.nextInt(100), "Team");
-        Players Player3 = new Players(random.nextInt(100), "Team");
-        Players Player4 = new Players(random.nextInt(100), "Team");
 
-        Players Player5 = new Players(random.nextInt(100), "Team");
-        Players Player6 = new Players(random.nextInt(100), "Team");
-        Players Player7 = new Players(random.nextInt(100), "Team");
-        Players Player8 = new Players(random.nextInt(100), "Team");
+        /*
+         * Creating the Player arrays 1 -> 4 for 4 teams, then calling the 
+         * getRandomName()' function 11 times for the 11 players on each team.
+        */
         
-        Players Player9 = new Players(random.nextInt(100), "Team");
-        Players Player10 = new Players(random.nextInt(100), "Team");
-        Players Player11= new Players(random.nextInt(100), "Team");
-        Players Player12= new Players(random.nextInt(100), "Team");
+        //Player list1 Creation
+        ArrayList<Players> team1Players = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            String randomPlayerName1 = getRandomPlayerName();
+            team1Players.add(new Players(1, randomPlayerName1));
+        }
+
+        //Player list2 Creation
+        ArrayList<Players> team2Players = new ArrayList<>();
+        for (int j = 0; j < 11; j++) {
+            String randomPlayerName2 = getRandomPlayerName();
+            team2Players.add(new Players(1, randomPlayerName2));
+        }
+
+        //Player list3 Creation
+        ArrayList<Players> team3Players = new ArrayList<>();
+        for (int k = 0; k < 11; k++) {
+            String randomPlayerName3 = getRandomPlayerName();
+            team3Players.add(new Players(1, randomPlayerName3));
+        }
+
+        //Player list4 Creation
+        ArrayList<Players> team4Players = new ArrayList<>();
+        for (int l = 0; l < 11; l++) {
+            String randomPlayerName4 = getRandomPlayerName();
+            team4Players.add(new Players(1, randomPlayerName4));
+        }
+
+        /*
+         * Creating the Team objects 1 -> 4 for the team4Players ArrayLists 1 -> 4, 
+         * then calling the getRandomTeamName()' function for each team name.
+        */
+
+        //Team1 Creation
+        String randomTeamName1 = getRandomTeamName();
+        Team team1 = new Team(team1Players, getRandomTeamName());
+
+        //Team2 Creation
+        String randomTeamName2 = getRandomTeamName();
+        Team team2 = new Team(team2Players, getRandomTeamName());
+
+        //Team3 Creation
+        String randomTeamName3 = getRandomTeamName();
+        Team team3 = new Team(team3Players, getRandomTeamName());
+
+        //Team4 Creation
+        String randomTeamName4 = getRandomTeamName();
+        Team team4 = new Team(team4Players, getRandomTeamName());
+    }
+
+    public static String getRandomPlayerName() {
+        String[] Playernames = {"John", "Alex", "Sam", "Tom", "James", "Michael", "David", "Chris", "Daniel", "Ryan", "Nick", "Andrew", "Jason"};
+        Random random = new Random();
+        return Playernames[random.nextInt(Playernames.length)];
+    }
+
+    public static String getRandomTeamName() {
+        String[] Teamnames = {"The Thunderbolts", "The Dragons", "The Titans", "The Mavericks", "The Wildcats", "The Sharks", "The Phoenixes", "The Warriors", "The Falcons", "The Hurricanes", "The Knights", "The Bears", "The Avengers", "The Guardians", "The Spartans", "The Outlaws"};
+        Random random = new Random();
+        return Teamnames[random.nextInt(Teamnames.length)];
+    }
+}
         
-        Players Player13 = new Players(random.nextInt(100), "Team");
-        Players Player14 = new Players(random.nextInt(100), "Team");
-        Players Player15 = new Players(random.nextInt(100), "Team");
-        Players Player16 = new Players(random.nextInt(100), "Team");
-}
-}
