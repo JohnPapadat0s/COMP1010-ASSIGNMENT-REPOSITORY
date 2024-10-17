3d408302cd1614381be4f4ea4c98d3a64a6b5649
HarrisonCove0 <harrison.cove@students.mq.edu.au>
2024-10-17 13:28:52 +1100

    Names

--- SRC/Client.java
+++ SRC/Client.java
@@ -74,8 +74,19 @@ public class Client {
 //Code to get the random player name that is called everytime the for loop is run when declaring each new player
     public static String getRandomPlayerName() {
         //This is the list of all possible names that can be chosen from
-        String[] Playernames = {"John", "Alex", "Sam", "Tom", "James", "Michael", "David", "Chris", "Daniel", "Ryan", "Nick", "Andrew", "Jason"};
-        //This is just the random function that is nescesary, Google said idk
+        String[] Playernames = {
+            "John", "Alberto", "Harry", "Sid", "James", "Michael", "David", "Chris", "Daniel", "Ryan", "Nick", "Andrew", "Jason", "Alex", "Matthew", 
+            "Ethan", "Samuel", "Oliver", "Ben", "Liam", "Kevin", "Scott", "Adam", "Peter", "Mark", "Eric", "Lucas", "Joseph", "Anto", "Jacob", "Thomas", 
+            "Jaiden", "Alessio", "Nathaniel", "Isaac", "Caleb", "Simon", "Victor", "Owen", "Julian", "Mason", "Gavin", "Charlie", "Henry", "Zachary", 
+            "Dylan", "Aiden", "Max", "Tristan", "Sebastian", "Felix", "Jett", "Leo", "Nathan", "Cole", "Jason", "Isaac", "Grant", "Tyler", "Marcus", 
+            "Jude", "Evan", "Blake", "Ashton", "Chase", "Wyatt", "Carter", "Riley", "Harrison", "Finn", "Miles", "Jordan", "Everett", "Kyle", "Josiah", 
+            "Malcolm", "Reese", "Soren", "Nolan", "Tyson", "Archer", "Rowan", "Silas", "Quinn", "Angelo", "Tristan", "Bruno", "Kingston", "Mason", 
+            "Weston", "Ryder", "Ashton", "Orion", "Zachary", "Dean", "Jorge", "Cole", "Marco", "Victor", "Damian", "Adam", "Jaxon", "Luca", "Eli", "Sawyer", 
+            "Maxwell", "Gideon", "Ezra", "Zane", "Malachi", "Hugo", "Landon", "Cash", "Kaden", "Ronan", "Dante", "Apollo", "Cyrus", "Tobias", "Remy", "Dorian", 
+            "Jasper", "Nico", "Finnian", "Theo", "Sullivan", "Bodhi", "Kian", "Bennett", "Tate", "Riley", "Silas", "August", "Emilio", "Wesley", "Cohen", 
+            "Jettson", "Kairo", "Ryder", "Fletcher", "Reed", "Zayden", "Hendrix", "Zeke", "Kendrick", "Jovany", "Brayden", "Talon", "Dawson"
+        };
+         //This is just something that is nescesary, Google said idk
         Random random = new Random();
         //Here the players name is returned, it returns a random string from the Playernames array return Playernames[], 
         //and it chooses a random number between 1 and the size of the Playernames array, random.nextInt(Playernames.length)
@@ -86,8 +97,18 @@ public class Client {
 //Code to get the random team name that is called everytime the for loop is run when declaring each new team
     public static String getRandomTeamName() {
         //This is the list of all possible names that can be chosen from
-        String[] Teamnames = {"The Thunderbolts", "The Dragons", "The Titans", "The Mavericks", "The Wildcats", "The Sharks", "The Phoenixes", "The Warriors", "The Falcons", "The Hurricanes", "The Knights", "The Bears", "The Avengers", "The Guardians", "The Spartans", "The Outlaws"};
-        //This is just the random function that is nescesary, Google said idk
+        String[] Teamnames = {
+            "The University of Sydney", "The University of Queensland", "Griffith University", "Macquarie University", "The University of Western Australia", 
+            "James Cook University", "Victoria University", "The University of New South Wales", "The University of Adelaide", "The University of Tasmania", 
+            "Deakin University", "The University of Melbourne", "Australian National University", "La Trobe University", "University of South Australia", 
+            "Central Queensland University", "The University of Newcastle", "Southern Cross University", "Curtin University", "Charles Sturt University", 
+            "Federation University Australia", "Edith Cowan University", "Western Sydney University", "Bond University", "University of the Sunshine Coast", 
+            "The University of Notre Dame Australia", "Australian Catholic University", "University of Southern Queensland", "Griffith College", 
+            "Torrens University Australia", "Queensland University of Technology", "RMIT University", "University of Wollongong", 
+            "Melbourne Institute of Technology", "Open Universities Australia", "TAFE Queensland", "Curtin College", "Swinburne University of Technology", 
+            "Australian Institute of Music", "Australian College of Applied Psychology", "Box Hill Institute", "Monash University"
+        };
+        //This is just something that is nescesary, Google said idk
         Random random = new Random();
         //Here the team name is returned, it returns a random string from the Teamnames array return Teamnames[], 
         //and it chooses a random number between 1 and the size of the Teamnames array, random.nextInt(Teamnames.length)
